9277582505480cc0f2821e9da64a7a3a5b4b1e7c
Priyansh Jain <priyansh.jain0246@gmail.com>
2018-10-20 17:25:43 +0530

    removed sql comments

--- ecom_store.sql
+++ ecom_store.sql
@@ -1,20 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 27, 2017 at 12:31 PM
--- Server version: 5.6.21
--- PHP Version: 5.5.19
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `ecom_store`
