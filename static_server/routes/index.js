a5d8a26a2c10490ccc8e9d20bbf550407df5445d
Priyansh Jain <priyansh.jain0246@gmail.com>
2018-10-29 19:08:01 +0530

    added sample server code

--- static_server/routes/index.js
+++ static_server/routes/index.js
@@ -1,12 +1,25 @@
 var express = require("express");
 var router = express.Router();
+const path = require("path");
+const Jimp = require("jimp");
+const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/image/:filename", (req, res, next) => {
-  res.sendFile("../images/" + req.params.filename);
+router.get("/image/test/:imagefile", (req, res, next) => {
+  Jimp.read(
+    path.join(__dirname, "..", "images", req.params.imagefile),
+    (err, lenna) => {
+      if (err) res.json({ success: false, err: err });
+      lenna
+        .resize(parseInt(req.query.height), parseInt(req.query.width)) // resize
+        .write("output.jpg"); // save
+      res.download(path.join(__dirname, "..", "output.jpg"));
+    }
+  );
 });
+
 module.exports = router;
